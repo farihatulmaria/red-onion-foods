80f1d8d7ce26d89e9dd93ade877916b0efbe2ab0
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2022-10-14 12:41:52 +0600

    some updates

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -2,14 +2,23 @@ import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { signOut } from 'firebase/auth';
 import React from 'react';
-import { Button, Container, Nav, Navbar, NavLink } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar, NavDropdown, NavLink } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link } from 'react-router-dom';
 import auth from '../../firebase';
 import logo2 from '../../img/logos/logo2.png';
 import userImg from '../../img/userImg.png';
 import './Header.css';
 const Header = () => {
     const [user] = useAuthState(auth);
+    const mainMenuItems = <>
+        <NavLink><Link to={'/'}>Home</Link></NavLink>
+        <NavLink><Link to={'/about'}>About</Link></NavLink>
+        <NavDropdown title="Menu">
+            <NavDropdown.Item><Link to={"/fast-food-Menu"}>Fast Food Menu</Link></NavDropdown.Item>
+            <NavDropdown.Item><Link to={"/restaurant-food-Menu"}>Restaurant Food Menu</Link></NavDropdown.Item>
+        </NavDropdown>
+    </>
     return (
         <header>
             <Navbar fixed='top' expand="lg" className='py-3'>
@@ -19,30 +28,30 @@ const Header = () => {
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ms-auto align-items-center text-right">
-                        <NavLink href="/cart">
-                            <FontAwesomeIcon icon={faCartShopping} />
-                        </NavLink>
-                        {
-                            user 
-                            ? 
-                            <>
-                            <NavLink href='profile'>
-                                <img src={user?.photoURL? user?.photoURL : userImg} alt="" width='50px'/>
-                            </NavLink>
-                            <NavLink>
-                                <Button onClick={()=>signOut(auth)}>Sign Out</Button>
-                            </NavLink>
-                            </>
-                            :
-                            <>
-                                <NavLink href="/login">Login</NavLink>
-                                <NavLink href='/sign-up'>
-                                    <Button>Sign Up</Button>
+                        <Nav className="ms-auto align-items-center text-right">
+                            {mainMenuItems}
+                            {
+                                user 
+                                ? 
+                                <>
+                                <NavLink href='/profile'>
+                                    <img src={user?.photoURL? user?.photoURL : userImg} alt="" width='50px'/>
                                 </NavLink>
-                            </>
-                        }
-                    </Nav>
+                                <NavLink href="/cart">
+                                    <FontAwesomeIcon icon={faCartShopping} />
+                                </NavLink>
+                                <NavLink>
+                                    <Button onClick={()=>signOut(auth)}>Sign Out</Button>
+                                </NavLink>
+                                </>
+                                :
+                                <>
+                                    <NavLink href='/login'>
+                                        <Button>Login</Button>
+                                    </NavLink>
+                                </>
+                            }
+                        </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
